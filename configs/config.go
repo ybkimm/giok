c34a4e99c209d8b2b0ae458432398a61dd3c722b
Yongbin Kim <iam@yongbin.kim>
2021-02-21 00:35:50 +0900

    Added configuration files

---
+++ configs/config.go
@@ -0,0 +1,8 @@
+package configs
+
+var Config = map[string]interface{}{
+	"Site": map[string]interface{}{
+		"Name":      "위키",
+		"Copyright": "&copy; Yongbin kim",
+	},
+}
---
+++ configs/debug_dev.go
@@ -0,0 +1,9 @@
+// +build !production
+
+package configs
+
+const (
+	Debug = true
+
+	DatabaseFile = "./giok.db"
+)
